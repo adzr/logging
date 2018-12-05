06b3318d99b5d1eddcb55d268d7709917e95e37c
Ahmed Zaher <adzr@users.noreply.github.com>
2018-12-06 01:05:44 +0200

    Refactoring, commenting code and updating dependencies.

--- go.mod
+++ go.mod
@@ -1,15 +1,17 @@
 module github.com/adzr/logging
 
 require (
+	github.com/VividCortex/gohistogram v1.0.0 // indirect
 	github.com/beorn7/perks v0.0.0-20180321164747-3a771d992973 // indirect
-	github.com/go-kit/kit v0.7.0
-	github.com/go-logfmt/logfmt v0.3.0 // indirect
+	github.com/go-kit/kit v0.8.0
+	github.com/go-logfmt/logfmt v0.4.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/gogo/protobuf v1.1.1 // indirect
 	github.com/golang/protobuf v1.2.0 // indirect
-	github.com/kr/logfmt v0.0.0-20140226030751-b84e30acd515 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/prometheus/client_golang v0.8.0
+	github.com/prometheus/client_golang v0.9.1
 	github.com/prometheus/client_model v0.0.0-20180712105110-5c3871d89910 // indirect
-	github.com/prometheus/common v0.0.0-20180801064454-c7de2306084e // indirect
-	github.com/prometheus/procfs v0.0.0-20180920065004-418d78d0b9a7 // indirect
+	github.com/prometheus/common v0.0.0-20181120120127-aeab699e26f4 // indirect
+	github.com/prometheus/procfs v0.0.0-20181005140218-185b4288413d // indirect
+	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
 )
--- logger.go
+++ logger.go
@@ -35,9 +35,13 @@ const (
 )
 
 var (
-	syncWriterLock   sync.Mutex
-	stdoutSyncWriter io.Writer
-	stderrSyncWriter io.Writer
+	// these are instances for std synchronized writers.
+	// they only need to be initialized once cause we
+	// don't want writers to be racing on writing
+	// to stdout and stderr.
+	stdoutSyncWriter, stderrSyncWriter io.Writer
+	// and this is to make sure of that.
+	initializeWritersOnce sync.Once
 )
 
 // Config carries service logging configuration.
@@ -57,10 +61,15 @@ func Configuration() *Config {
 	}
 }
 
+// checks if the logger is configured not to log anything.
 func isLevelNone(l string) bool {
 	return "none" == strings.ToLower(strings.TrimSpace(l))
 }
 
+// checks if the specified level string matches to
+// a valid logger level and returns it if it does,
+// else it returns "AllowAll" which lets all
+// logs to go through.
 func getValidLevel(l string) level.Option {
 	switch strings.ToLower(strings.TrimSpace(l)) {
 	case "error":
@@ -76,32 +85,38 @@ func getValidLevel(l string) level.Option {
 	}
 }
 
-func createLoggerTypeFactory(loggerType string) func(io.Writer) log.Logger {
+// takes a format-type string and returns a factory
+// that creates a non-filtered logger with a writer.
+func createLoggerFactory(loggerType string) func(io.Writer) log.Logger {
 	switch strings.ToLower(strings.TrimSpace(loggerType)) {
 	default:
 		return log.NewJSONLogger
 	}
 }
 
-func assertWriters() {
-	defer syncWriterLock.Unlock()
-	syncWriterLock.Lock()
-
-	if stdoutSyncWriter == nil {
-		stdoutSyncWriter = log.NewSyncWriter(os.Stdout)
-	}
-
-	if stderrSyncWriter == nil {
-		stderrSyncWriter = log.NewSyncWriter(os.Stderr)
-	}
-}
-
+// returns new synchronized stdOut & stdErr loggers based on the specified logger factory.
 func createSyncStdLoggers(loggerTypeFactory func(io.Writer) log.Logger) (log.Logger, log.Logger) {
-	assertWriters()
+
+	// initialize the writers only once.
+	initializeWritersOnce.Do(func() {
+		if stdoutSyncWriter == nil {
+			stdoutSyncWriter = log.NewSyncWriter(os.Stdout)
+		}
+
+		if stderrSyncWriter == nil {
+			stderrSyncWriter = log.NewSyncWriter(os.Stderr)
+		}
+	})
+
+	// now, we can use the writers to return as many loggers as we want by just calling the function.
 	return log.With(loggerTypeFactory(stdoutSyncWriter), "ts", log.DefaultTimestampUTC),
 		log.With(loggerTypeFactory(stderrSyncWriter), "ts", log.DefaultTimestampUTC, "caller", log.Caller(5))
 }
 
+// this is to keep track of how many log entries has been sent
+// to each logger since we intend to use a separate logger
+// for errors and another for the rest of the logs.
+// let's call these two loggers "appenders".
 type multiAppenderInstrumentedLogger struct {
 	loggers map[level.Value]log.Logger
 	counter metrics.Counter
@@ -110,13 +125,20 @@ type multiAppenderInstrumentedLogger struct {
 
 func (l *multiAppenderInstrumentedLogger) Log(keyvals ...interface{}) error {
 
+	// here we loop through keys and values.
 	for i := 0; i < len(keyvals); i += 2 {
+		// check if this is the key that indicates the severity level of the log entry.
 		if k := keyvals[i]; k == level.Key() {
+			// if yes then get its value.
 			if v, ok := keyvals[i+1].(level.Value); ok {
+				// if we use a metrics counter then increment it for the resolved value.
 				if l.counter != nil {
 					l.counter.With("level", v.String()).Add(1)
 				}
 
+				// now if the loggers are defined - which they should be - get the logger
+				// that matches the severity level of the log entry and append the entry
+				// to that logger adding the logger name.
 				if l.loggers != nil {
 					if target := l.loggers[v.(level.Value)]; target != nil {
 						keyvals = append(keyvals, "logger", l.name)
@@ -136,23 +158,31 @@ func (l *multiAppenderInstrumentedLogger) Log(keyvals ...interface{}) error {
 // logs nor monitoring will take place.
 func CreateStdSyncLogger(loggerName string, counter metrics.Counter, config *Config) log.Logger {
 
+	// if you're required to log nothing, then just return a dummy logger.
 	if isLevelNone(config.Level) {
 		return log.NewNopLogger()
 	}
 
+	// else get the severity level required.
 	lvl := getValidLevel(config.Level)
 
-	// Create two loggers for stdout and stderr based on logger type chosen.
-	outLogger, errLogger := createSyncStdLoggers(createLoggerTypeFactory(config.Format))
+	// create two "appenders" for stdout and stderr based on the factory chosen.
+	outLogger, errLogger := createSyncStdLoggers(createLoggerFactory(config.Format))
 
+	// create a filter for the stdout "appender" based on the resolved severity level.
 	outLogger = level.NewFilter(outLogger, lvl)
 
+	// now, create a map for the defined appenders matching each severity level.
 	loggers := make(map[level.Value]log.Logger)
 
+	// errors should only go to stderr.
 	loggers[level.ErrorValue()] = errLogger
+
+	// the rest to stdout
 	loggers[level.WarnValue()] = outLogger
 	loggers[level.InfoValue()] = outLogger
 	loggers[level.DebugValue()] = outLogger
 
+	// finally return an instrumented wrapping logger for the appenders we've created.
 	return &multiAppenderInstrumentedLogger{name: loggerName, loggers: loggers, counter: counter}
 }
